97f89ce1b364555fc6d58e129ce9009b800ab81b
itszw <itszwtr@gmail.com>
2022-04-08 14:42:19 +0300

    a
    a

--- Soundama.lua
+++ Soundama.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/printer.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/Sound.lua"))()
