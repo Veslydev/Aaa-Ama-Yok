498027ca39bb92377cdf428046ec927a22811d3d
itszw <itszwtr@gmail.com>
2022-04-08 14:45:09 +0300

    launcher amk

---
+++ Launcher.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/AllAma.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/Text.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/Soundama.lua"))()
+loadstring(game:HttpGetAsync("hhttps://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/OnlyAma.lua"))()
