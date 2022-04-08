e2ebb192d186ee801ebbebd124137339e78325ea
itszw <itszwtr@gmail.com>
2022-04-08 15:23:31 +0300

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -1,4 +1,7 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/AllAma.lua"))()
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/Text.lua"))()
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/Soundama.lua"))()
-loadstring(game:HttpGetAsync("hhttps://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/OnlyAma.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/AllAma.lua"))() -- Ufo  Open All // Ufo Hepsi True
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/Text.lua"))()  -- Ufo Only Text Open // Ufo Sadece Text True
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/Soundama.lua"))() -- Ufo Only Sound Open // Ufo Sadece Sound True
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/OnlyAma.lua"))() -- Ufo All False // Ufo Hepsi False
