af0b308e2b0206696dace4978844204335a5e91b
itszw <itszwtr@gmail.com>
2022-04-09 03:11:29 +0300

    a
    a

--- Kaba.lua
+++ Kaba.lua
@@ -1,7 +1,7 @@
 
 -- init
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/UI-Libraries/main/Venyx/Source.lua"))()
-local venyx = library.new("Esx Ufo Hub", 6022668888)
+local venyx = library.new("Esx Hub", 6022668888)
 
 -- themes
 local themes = {
@@ -14,9 +14,9 @@ local themes = {
 }
 
 -- first page
-local page = venyx:addPage("Ufo Options", 6022668888)
-local section1 = page:addSection("Ufo Options")
-local section2 = page:addSection("Section 2")
+local page = venyx:addPage("Esx Scripts", 6022668888)
+local section1 = page:addSection("Ufo")
+local section2 = page:addSection("Boombox")
 
 section1:addButton("All", function()
 	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/AllAma.lua"))()
@@ -28,7 +28,7 @@ section1:addButton("Text", function()
 	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/Text.lua"))()
 end)
 section1:addButton("Only", function()
-	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/OnlyAma.lua"))()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/OnlyAma.lua"))()
 
 
 	if focusLost then
@@ -36,41 +36,32 @@ section1:addButton("Only", function()
 	end
 end)
 
-section2:addColorPicker("ColorPicker", Color3.fromRGB(50, 50, 50))
-section2:addColorPicker("ColorPicker2")
-section2:addSlider("Slider", 0, -100, 100, function(value)
-	print("Dragged", value)
+section2:addButton("We Boombox Gui", function()
+    loadstring(game:GetObjects("rbxassetid://6952389432")[1].Source)()
 end)
-section2:addDropdown("Dropdown", {"Hello", "World", "Hello World", "Word", 1, 2, 3})
-section2:addDropdown("Dropdown", {"Hello", "World", "Hello World", "Word", 1, 2, 3}, function(text)
-	print("Selected", text)
-end)
-section2:addButton("Button")
-
--- second page
-local theme = venyx:addPage("Settings", 6022668888)
-local ayar =  theme:addSection("Ayarlar")
-local fps =  theme:addSection("Fps")
-local colors = theme:addSection("Colors")
-
-ayar:addKeybind("Menu Key", Enum.KeyCode.RightShift, function()
-	print("Aktive Key")
-	venyx:toggle()
-end, function()
-	print("Key Degisti")
-end)
-
-section5:addButton("Unlock", function()
-	print ("itszw#2241")
-
-for theme, color in pairs(themes) do -- all in one theme changer, i know, im cool
-	colors:addColorPicker(theme, color, function(color3)
-		venyx:setTheme(theme, color3)
-	end)
-end
-
 -- load
 venyx:SelectPage(venyx.pages[1], true)
+local page4 = venyx:addPage("Esx Team Using But Not Owned", 6022668888)
+local Adminsscript = page4:addSection("Admins")
+FunnyTroll:addButton("ifyield", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+local Finger = page4:addSection("Finger")
+local FunnyTroll = page4:addSection("Funny / Troll / Boombox")
+FunnyTroll:addButton("Vets Hub", function()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/3JBH/Boombox-Scripts/main/Verts-Hub/Verts-Hub-Source.lua"))()
+end)
+
+
+
+
+
+
+
+
+
+
+
 -- Third
 local page3 = venyx:addPage("Credits", 5012544693)
 local section5 = page3:addSection("-----------------------------Creditler------------------------")
@@ -86,3 +77,26 @@ section5:addButton("Vynixius", function()
 	print ("RegularVynixu#8039")
     print("Vynixu Dc:discord.gg/uP2Nnfn9MG")
     end)
+
+-- second page
+local theme = venyx:addPage("Settings", 5012544693)
+local ayar =  theme:addSection("Ayarlar")
+local fps =  theme:addSection("Fps")
+local colors = theme:addSection("Colors")
+
+ayar:addKeybind("Menu Key", Enum.KeyCode.RightShift, function()
+	print("Aktive Key")
+	venyx:toggle()
+end, function()
+	print("Key Degisti")
+end)
+
+fps:addButton("Unlock", function ()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/itszw/Aaa-Ama-Yok/main/fps"))()
+end)
+
+for theme, color in pairs(themes) do -- all in one theme changer, i know, im cool
+	colors:addColorPicker(theme, color, function(color3)
+		venyx:setTheme(theme, color3)
+	end)
+end
