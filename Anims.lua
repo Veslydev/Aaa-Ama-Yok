c737dc223062307e62063762f62fe61945dffd68
itszw <74131882+itszw@users.noreply.github.com>
2022-11-27 19:53:05 +0300

    Update Anims.lua

--- Anims.lua
+++ Anims.lua
@@ -1,4 +1,4 @@
-while true do
+game:GetService"RunService".Stepped:connect(function()
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=1212900985"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=1150842221"
@@ -7,6 +7,4 @@ Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
 Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=656117878"
 Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=658360781"
 Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
-game.Players.LocalPlayer.Character.Humanoid.Jump = false
-wait(1)
-end
+end)
